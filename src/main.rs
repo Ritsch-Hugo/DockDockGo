4bbe24a6a3fc243be1f26dfaeff67b13971641cb
Antoine Moulin <antoine.moulin@isen.yncrea.fr>
2026-01-07 20:13:13 +0100

    Proxy fonctionnel via TAGS, Dashboard non conforme

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,25 @@ use std::sync::{Mutex};
 
 use serde_json::Value;
 
+use uuid::Uuid;
+
+#[derive(Clone)]
+struct PullContext 
+{
+    client_ip: String, // IP du client
+    client_id: Uuid,
+    repo: String,
+    tag: Option<String>,
+
+}
 struct ImageState {
     blobs_expected: HashSet<String>,  // tous les blobs attendus pour l'image
     blobs_downloaded: HashSet<String> // blobs déjà téléchargés
 }
 
 type SharedState = Arc<Mutex<HashMap<String, ImageState>>>;
+type PullMap = Arc<Mutex<HashMap<String, PullContext>>>;
+
 static UPSTREAM: &str = "https://registry-1.docker.io";
 
 /// 🔐 Politique de sécurité
@@ -118,18 +131,20 @@ fn sha256_hex(data: &[u8]) -> String {
 }
 
 /// 💾 Sauvegarde en quarantaine
-fn save_to_quarantine(path: &str, bytes: &[u8]) {
+fn save_to_quarantine(path: &str, bytes: &[u8], context: &PullContext) {
     let parts: Vec<&str> = path.trim_start_matches("/v2/").split('/').collect();
     if parts.len() < 4 {
         return;
     }
 
+    let tag = context.tag.as_deref().unwrap_or("unknown");
+    println!("[CACHE] Tag : {}", tag);
     let repo = format!("{}/{}", parts[0], parts[1]);
 
     // MANIFEST
     if parts[2] == "manifests" {
         let name = parts[3].trim_start_matches("sha256:");
-        let dir = format!("quarantaine/{}/manifests", repo);
+        let dir = format!("quarantaine/{}/{}/manifests", repo, tag);
         create_dir_all(&dir).ok();
         fs::write(format!("{}/{}.json", dir, name), bytes).ok();
     }
@@ -137,21 +152,21 @@ fn save_to_quarantine(path: &str, bytes: &[u8]) {
     // BLOB
     if parts[2] == "blobs" && parts[3].starts_with("sha256:") {
         let digest = parts[3].trim_start_matches("sha256:");
-        let dir = format!("quarantaine/{}/blobs/sha256", repo);
+        let dir = format!("quarantaine/{}/{}/blobs/sha256", repo, tag);
         create_dir_all(&dir).ok();
         fs::write(format!("{}/{}", dir, digest), bytes).ok();
     }
     // REFERRERS
     if parts[2] == "referrers" && parts[3].starts_with("sha256:") {
         let digest = parts[3].trim_start_matches("sha256:");
-        let dir = format!("quarantaine/{}/referrers", repo);
+        let dir = format!("quarantaine/{}/{}/referrers", repo, tag);
         create_dir_all(&dir).ok();
         fs::write(format!("{}/{}.json", dir, digest), bytes).ok();
     }
 }
 
 /// 📦 Sert depuis le cache qui contient les images préalablement scannées
-fn try_serve_from_cache(req: &Request<Body>) -> Option<Response<Body>> {
+fn try_serve_from_cache(req: &Request<Body>, context: &PullContext) -> Option<Response<Body>> {
     let path = req.uri().path();
     let is_head = req.method() == Method::HEAD;
 
@@ -160,89 +175,170 @@ fn try_serve_from_cache(req: &Request<Body>) -> Option<Response<Body>> {
         return None;
     }
 
+    //decapsuler le tag du contexte
+    let tag = context.tag.as_deref().unwrap_or("unknown");
+    println!("[CACHE] Tag : {}", tag);
+
     let repo = format!("{}/{}", parts[0], parts[1]);
 
     println!("[CACHE] Recherche de {} dans {}", path, repo);
-    println!("parts: {:?}", parts[1]);
 
-    // ===== MANIFEST =====
-    if parts[2] == "manifests" {
-        let name = parts[3].trim_start_matches("sha256:");
-        let file = format!("cache/{}/manifests/{}.json", repo, name);
-        //si le fichier demandé par la requete existe et a pu etre lu
-        if let Ok(data) = fs::read(&file) {
-            println!("data: {}", String::from_utf8_lossy(&data));
-            if data.len() < 20 {
-                return None;
-            }
-            
-            let digest = sha256_hex(&data);//calcul du digest sha256 du manifest
-
-            //construction de la reponse HTTP conforme au standard Docker Registry v2
-            return Some(
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Docker-Distribution-API-Version", "registry/2.0")
-                    .header(
-                        "Content-Type",
-                        "application/vnd.docker.distribution.manifest.v2+json",
-                    )
-                    .header("Docker-Content-Digest", format!("sha256:{digest}"))
-                    .header("Content-Length", data.len())
-                    .body(if is_head { Body::empty() } else { Body::from(data) })
-                    .unwrap(),
-            );
-        }
-    }
-
-    // ===== BLOB =====
-    if parts[2] == "blobs" && parts[3].starts_with("sha256:") {
-        let digest = parts[3].trim_start_matches("sha256:");
-        let file = format!("cache/{}/blobs/sha256/{}", repo, digest);
-
-        if let Ok(data) = fs::read(&file) {
-            let real_digest = sha256_hex(&data);
-
-            return Some(
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Content-Type", "application/octet-stream")
-                    .header("Docker-Content-Digest", format!("sha256:{real_digest}"))
-                    .header("Content-Length", data.len())
-                    .body(if is_head { Body::empty() } else { Body::from(data) })
-                    .unwrap(),
-            );
-        }
-    }
-    
-    if parts[2] == "referrers" && parts[3].starts_with("sha256:") 
+    //On doit laisser passer les requetues GET a chaques fois jusqu'au dernier blob meme si l'image n'est pas en cache
+    if req.method() == Method::GET
     {
-        let digest = parts[3].trim_start_matches("sha256:");
-        let file = format!("cache/{}/referrers/{}.json", repo, digest);
+        // ===== MANIFEST =====
+        if parts[2] == "manifests" 
+        {
+            let name = parts[3].trim_start_matches("sha256:");
+            let file = format!("cache/{}/{}/manifests/{}.json", repo, tag, name);
+            //si le fichier demandé par la requete existe et a pu etre lu
+            if let Ok(data) = fs::read(&file) 
+            {
+                println!("data: {}", String::from_utf8_lossy(&data));
+                if data.len() < 20 {
+                    return None;
+                }
+                
+                let digest = sha256_hex(&data);//calcul du digest sha256 du manifest
 
-        if let Ok(data) = fs::read(&file) {
-            // Sécurité minimale : referrers non vide et JSON plausible
-            if data.len() < 20 {
-                return None; // image incomplète → blocage
+                //construction de la reponse HTTP conforme au standard Docker Registry v2
+                return Some(
+                    Response::builder()
+                        .status(StatusCode::OK)
+                        .header("Docker-Distribution-API-Version", "registry/2.0")
+                        .header(
+                            "Content-Type",
+                            "application/vnd.docker.distribution.manifest.v2+json",
+                        )
+                        .header("Docker-Content-Digest", format!("sha256:{digest}"))
+                        .header("Content-Length", data.len())
+                        .body(if is_head { Body::empty() } else { Body::from(data) })
+                        .unwrap(),
+                );
+            
             }
+        }
 
-            return Some(
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Docker-Distribution-API-Version", "registry/2.0")
-                    .header("Content-Type", "application/vnd.oci.image.index.v1+json")
-                    .header("Content-Length", data.len())
-                    .body(if is_head { Body::empty() } else { Body::from(data) })
-                    .unwrap(),
-            );
+        // ===== BLOB =====
+        if parts[2] == "blobs" && parts[3].starts_with("sha256:") {
+            let digest = parts[3].trim_start_matches("sha256:");
+            let file = format!("cache/{}/{}/blobs/sha256/{}", repo, tag, digest);
+
+            if let Ok(data) = fs::read(&file) {
+                let real_digest = sha256_hex(&data);
+
+                return Some(
+                    Response::builder()
+                        .status(StatusCode::OK)
+                        .header("Content-Type", "application/octet-stream")
+                        .header("Docker-Content-Digest", format!("sha256:{real_digest}"))
+                        .header("Content-Length", data.len())
+                        .body(if is_head { Body::empty() } else { Body::from(data) })
+                        .unwrap(),
+                );
+            }
+        }
+        
+        if parts[2] == "referrers" && parts[3].starts_with("sha256:") 
+        {
+            let digest = parts[3].trim_start_matches("sha256:");
+            let file = format!("cache/{}/{}/referrers/{}.json", repo, tag, digest);
+
+            if let Ok(data) = fs::read(&file) {
+                // Sécurité minimale : referrers non vide et JSON plausible
+                if data.len() < 20 {
+                    return None; // image incomplète → blocage
+                }
+
+                return Some(
+                    Response::builder()
+                        .status(StatusCode::OK)
+                        .header("Docker-Distribution-API-Version", "registry/2.0")
+                        .header("Content-Type", "application/vnd.oci.image.index.v1+json")
+                        .header("Content-Length", data.len())
+                        .body(if is_head { Body::empty() } else { Body::from(data) })
+                        .unwrap(),
+                );
+            }
         }
     }
 
     None
 }
 
+fn manifest_list_has_linux_amd64(bytes: &[u8]) -> bool {
+    let v: serde_json::Value = match serde_json::from_slice(bytes) {
+        Ok(v) => v,
+        Err(_) => return false,
+    };
 
-async fn handle(req: Request<Body>, client: Client, state: SharedState) -> Response<Body> {
+    let manifests = match v.get("manifests").and_then(|m| m.as_array()) {
+        Some(m) => m,
+        None => return false,
+    };
+
+    for m in manifests {
+        let platform = match m.get("platform") {
+            Some(p) => p,
+            None => continue,
+        };
+
+        let os = match platform.get("os").and_then(|v| v.as_str()) {
+            Some(o) => o,
+            None => continue,
+        };
+
+        let arch = match platform.get("architecture").and_then(|v| v.as_str()) {
+            Some(a) => a,
+            None => continue,
+        };
+
+        if os == "linux" && arch == "amd64" {
+            return true;
+        }
+    }
+
+    false
+}
+
+
+
+fn get_pull_context(req: &Request<Body>, parts: &[&str], client_ip: &str, pull_map: &PullMap) -> PullContext 
+{
+    // Sécurité minimale
+    if parts.len() < 2 {
+        return PullContext {
+            client_ip: client_ip.to_string(),
+            client_id: Uuid::new_v4(),
+            repo: "unknown".to_string(),
+            tag: None,
+        };
+    }
+
+    let repo = format!("{}/{}", parts[0], parts[1]);
+    let key = format!("{}|{}", client_ip, repo);
+
+    let mut map = pull_map.lock().unwrap();
+    // Récupération ou création du contexte PullContext
+    let entry = map.entry(key).or_insert_with(|| PullContext {
+        client_ip: client_ip.to_string(),
+        client_id: Uuid::new_v4(),
+        repo: repo.clone(),
+        tag: None,
+    });
+
+    // Mise à jour du tag UNIQUEMENT sur HEAD manifest tag
+    if req.method() == Method::HEAD
+        && parts.len() > 3
+        && !parts[3].starts_with("sha256:")
+    {
+        entry.tag = Some(parts[3].to_string());
+    }
+
+    entry.clone()
+}
+
+async fn handle(req: Request<Body>, client: Client, state: SharedState, pull_map: PullMap) -> Response<Body> {
     let method = req.method().clone();
     let uri = req.uri().clone();
     let path = uri.path().to_string();
@@ -255,9 +351,52 @@ async fn handle(req: Request<Body>, client: Client, state: SharedState) -> Respo
         return Response::builder().status(StatusCode::OK).body(Body::empty()).unwrap();
     }
 
+
+    // Découper le path pour extraire repo, tag, digest
+    let parts: Vec<&str> = path.trim_start_matches("/v2/").split('/').collect();
+
+    
+    //recupère l'ip du client 
+    let client_ip = req
+    .extensions()
+    .get::<std::net::SocketAddr>()
+    .unwrap()
+    .ip()
+    .to_string();
+
+    /*if parts.len() > 3 {
+    println!("Requête HEAD sur tag : {}", parts[3]);
+    }*/
+
+    //recupère le contexte du pull en cours
+    let context = get_pull_context(&req, &parts, &client_ip, &pull_map);
+
+    //log
+    println!(
+        "[PULL CONTEXT] client={} uuid={} repo={} tag={:?} client_ip={}",
+        client_ip,
+        context.client_id,
+        context.repo,
+        context.tag,
+        context.client_ip
+    );
+
+
+    let requested_repo = format!("{}/{}", parts[0], parts[1]);
+
+    //Verification du contexte de requete
+    // /!\Produit une erreure si differents pulls sont mélangés 
+    // Solution -> Utiliser un Uuid de session de pull commun a chaques requetes d'un meme pull
+    if client_ip != context.client_ip || requested_repo != context.repo{
+        println!("[ERROR] IP du client ne correspond pas au contexte PullContext");
+        return Response::builder()
+            .status(StatusCode::FORBIDDEN)
+            .body(Body::from("IP du client ne correspond pas au contexte PullContext"))
+            .unwrap();
+    }
     // Tenter de servir depuis le cache local
     //Si la fonction try_serve_from_cache retourne une reponse (Some)
-    if let Some(resp) = try_serve_from_cache(&req) {
+    if let Some(resp) = try_serve_from_cache(&req, &context) {
         println!("[LOCAL REGISTRY] {}", path);
         return resp;
     }
@@ -308,9 +447,33 @@ async fn handle(req: Request<Body>, client: Client, state: SharedState) -> Respo
     let headers = upstream.headers().clone();
     let bytes = upstream.bytes().await.unwrap_or_default(); // consomme `upstream`
 
+    // Vérification architecture UNIQUEMENT sur GET manifest
+    if method == Method::GET
+        && path.contains("/manifests/")
+        && !bytes.is_empty()
+    {
+        // Si c'est une manifest list (présence du champ "manifests")
+        let is_manifest_list = serde_json::from_slice::<serde_json::Value>(&bytes)
+            .ok()
+            .and_then(|v| v.get("manifests").cloned())
+            .is_some();
+
+        if is_manifest_list && !manifest_list_has_linux_amd64(&bytes) {
+            println!("[ARCH CHECK] Manifest list incompatible linux/amd64");
+
+            return Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from(
+                    "No matching manifest for linux/amd64"
+                ))
+                .unwrap();
+        }
+    }
+
+
     // === POLICY CHECK ===
     if !is_allowed(&path, &bytes) {
-        save_to_quarantine(&path, &bytes); // stocke la ressource dans la quarantaine
+        save_to_quarantine(&path, &bytes, &context); // stocke la ressource dans la quarantaine
         return Response::builder()
             .status(StatusCode::FORBIDDEN)
             .body(Body::from("Image mise en quarantaine"))
@@ -320,7 +483,7 @@ async fn handle(req: Request<Body>, client: Client, state: SharedState) -> Respo
     // Sauvegarder les manifests et blobs GET valides dans la quarantaine
     if method == Method::GET && !bytes.is_empty() {
         if path.contains("/manifests/") || path.contains("/blobs/") || path.contains("/referrers/") {
-            save_to_quarantine(&path, &bytes);
+            save_to_quarantine(&path, &bytes, &context);
         }
     }
     // Vérifier si c'est la dernière requête à traiter pour cette image
@@ -361,6 +524,9 @@ async fn main() -> Result<()> {
 
     let client = Client::builder().use_rustls_tls().build()?;
 
+    // State partagé pour suivre les pulls en cours
+    let pull_map: PullMap = Arc::new(Mutex::new(HashMap::new()));
+
     // State partagé pour suivre les blobs/manifests
     let state: SharedState = Arc::new(Mutex::new(HashMap::new()));
 
@@ -370,16 +536,31 @@ async fn main() -> Result<()> {
         let (stream, addr) = listener.accept().await?;
         let acceptor = acceptor.clone();
         let client = client.clone();
-        let state = state.clone(); // on clone Arc pour le passer à la tâche
-
+        let state = state.clone(); // Arc<Mutex<SharedState>>
+        let pull_map = pull_map.clone(); // <-- clone pour cette itération
+        
         tokio::spawn(async move {
             println!("[CONN] Client {:?}", addr);
-            if let Ok(tls) = acceptor.accept(stream).await {
-                let service = service_fn(move |req| {
-                    let client = client.clone();
-                    let state = state.clone(); // passer state à handle
-                    async move { Ok::<_, Infallible>(handle(req, client, state).await) }
-                });
+            if let Ok(tls) = acceptor.accept(stream).await 
+            {
+                
+                let client = client.clone();
+                let state = state.clone();
+                let pull_map = pull_map.clone(); // <-- clone pour service_fn
+
+                let service = service_fn(move |mut req| 
+                    {
+                        let client = client.clone();
+                        let state = state.clone();
+                        let pull_map = pull_map.clone(); // clone pour handle
+                        let addr = addr; // passer addr
+                        async move 
+                        { 
+                            // stocker addr dans la requête pour handle
+                            req.extensions_mut().insert(addr);
+                            Ok::<_, Infallible>(handle(req, client, state, pull_map).await) 
+                        }
+                    });
                 let _ = Http::new().serve_connection(tls, service).await;
             }
         });
