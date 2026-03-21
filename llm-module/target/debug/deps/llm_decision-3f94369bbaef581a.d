5b831f527ab0b94b24d9c090b4a294804a096c41
Lenny Bouchard <bouchardlenny83@gmail.com>
2026-03-21 01:37:37 +0100

    fix: supprimer target/ du tracking git

--- llm-module/target/debug/deps/llm_decision-3f94369bbaef581a.d
+++
@@ -1,5 +0,0 @@
-/home/scuti/DocDockGo/llm-module/target/debug/deps/llm_decision-3f94369bbaef581a.d: llm-decision/src/main.rs
-
-/home/scuti/DocDockGo/llm-module/target/debug/deps/llm_decision-3f94369bbaef581a: llm-decision/src/main.rs
-
-llm-decision/src/main.rs:
--- llm-module/target/debug/deps/llm_manager-64860f43d44bd214.d
+++
@@ -1,5 +0,0 @@
-/home/scuti/DocDockGo/llm-module/target/debug/deps/llm_manager-64860f43d44bd214.d: llm-manager/src/main.rs
-
-/home/scuti/DocDockGo/llm-module/target/debug/deps/llm_manager-64860f43d44bd214: llm-manager/src/main.rs
-
-llm-manager/src/main.rs:
