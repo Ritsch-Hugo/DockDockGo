4d4526adc3080196664072a598112feec8768063
Ritsch-Hugo <hugo.ritsch.pro@outlook.fr>
2026-05-17 13:55:15 +0200

    feat: scaffold projet Rust cycle-de-vie (tokio, axum, serde, tracing)

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "cycle_de_vie=debug".into()),
+        )
+        .json()
+        .init();
+
+    info!(service = "cycle-de-vie", "Service starting");
+}
